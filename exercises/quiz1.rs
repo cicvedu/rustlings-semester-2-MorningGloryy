42d9b39497815e9a708a4bb64a4a4cdf0a22e842
pengyufan <1109818924@qq.com>
2023-10-23 23:00:32 -0700

    2023.10.24 -strings Progress:[########################>---------------------]41/100 (40.0 %)

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(test: u32) -> u32 {
+    if test <= 40 {
+        test * 2
+    } else {
+        test
+    }
 
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
